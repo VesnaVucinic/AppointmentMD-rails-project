1151a563f85f0721d2656d946d2b25af89208ef8
amitnyc83 <amit_nyc@live.com>
2018-12-20 14:09:12 -0500

    create a session id for patients & physicians

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,27 @@
 class SessionsController < ApplicationController
   def new
+    @patient = Patient.new
+    @physician = Physician.new
   end
 
   def create
+    patient = Patient.find_by(name: params[:name])
+    if patient && patient.authenticate(params[:password])
+      session[:patient.id] = patient.id
+      redirect_to patient_path(patient)
+    elsif
+      physician = Physician.find_by(name: params[:name])
+      if physician && physician.authenticate(params[:password])
+      sessions[:physician.id] = physician.id
+      redirect_to physician_path(physician)
+    else
+      render 'session/new'
   end
 
   def destroy
+    session.delete :patient.id || session.delete :physician.id
+    redirect_to root_path
   end
+
+
 end
